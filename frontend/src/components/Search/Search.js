717ad8b77d8a0150b8425acd5bc5341130a00fc1
Andrei Skiruta <skiruta.andrei@mail.ru>
2024-11-08 14:01:54 +0700

    11 - added search
    Added search, changed styles of cards, set up Context API for search

---
+++ frontend/src/components/Search/Search.js
@@ -0,0 +1,18 @@
+import { HiMagnifyingGlass } from "react-icons/hi2"
+import { useContext } from "react"
+import SearchContext from "./SearchContext"
+
+function Search() {
+  const search = useContext(SearchContext)
+
+  return (
+    <div>
+      <form>
+        <HiMagnifyingGlass />
+        <input onChange={(e) => search.setValue(e.target.value)}></input>
+      </form>
+    </div>
+  )
+}
+
+export default Search
